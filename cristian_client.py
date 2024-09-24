0246d314aa76f3525ce87f601068e578ccc1eae8
Elliot Duran <elliotd70@outlook.com>
2024-09-24 17:29:44 -0600

    Cristian algorithm

---
+++ cristian_client.py
@@ -0,0 +1,38 @@
+import socket
+import time
+import datetime
+
+
+def str_to_datetime(x: str) -> datetime:
+    return datetime.datetime.strptime(x, '%Y%m%d%H:%M:%f')
+
+
+HOST = "localhost"
+PORT = 9099
+
+
+sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+t0 = time.time()
+sc.connect((HOST, PORT))
+server_time = sc.recv(4096).decode()
+t1 = time.time()
+
+Cs: datetime = str_to_datetime(server_time)
+Cc: datetime = datetime.datetime.now()
+c = Cs + datetime.timedelta(seconds=(t1-t0)/2)
+
+print("Server time", Cs)
+print("Client time before adjustment", Cc)
+print("Exact time is", Cs+datetime.timedelta(seconds=t1-t0))
+
+if c < Cc:
+    aux = Cc-c
+    time.sleep(aux.total_seconds() / (24 * 3600))
+    Cc = datetime.datetime.now()
+else:
+    Cc = c
+
+print("Client time after adjustment", Cc)
+
+sc.close()
---
+++ cristian_server.py
@@ -0,0 +1,19 @@
+import socket
+import datetime
+
+HOST = "localhost"
+PORT = 9099
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind((HOST, PORT))
+ss.listen()
+
+while True:
+    conn, addr = ss.accept()
+    print("Connecting with client ", addr)
+
+    date = datetime.datetime.now() # Cs
+    date_str = date.strftime('%Y%m%d%H:%M:%f')
+    print("Sending time to ", addr)
+    print(date)
+    conn.send(date_str.encode())
+    conn.close()
